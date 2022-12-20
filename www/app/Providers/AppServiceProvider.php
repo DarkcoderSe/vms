e659d907914035d8257e9737a1d1bf93a29c8534
DarkcoderSe <kashifs294@gmail.com>
2022-12-20 08:14:20 -0500

    laravel version upgraded to 8.0 from 6.2; php 8.1 support added with passport 10

--- www/app/Providers/AppServiceProvider.php
+++ www/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::withoutComponentTags();
+
+
     }
 }
--- www/config/session.php
+++ www/config/session.php
@@ -166,8 +166,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
-
+    'secure' => env('SESSION_SECURE_COOKIE', null),
     /*
     |--------------------------------------------------------------------------
     | HTTP Access Only
